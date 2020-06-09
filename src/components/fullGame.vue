a911f757926cc9ae3c9f2dd579d319b093ddb2df
Gerti Goga <gertigoga@gmail.com>
2020-06-08 20:19:51 -0700

    Added player action bar

--- src/components/fullGame.vue
+++ src/components/fullGame.vue
@@ -2,29 +2,32 @@
   <div>
     <Healthbar class="inline" :player="players[0]" :health="playerHealth" />
     <Healthbar class="inline" :player="players[1]" :health="monsterHealth" />
+    <PlayerActions />
   </div>
 </template>
 
 <script>
 import Healthbar from "./healthbar";
+import PlayerActions from "./playerActions"
 export default {
   data: function() {
     return {
       players: ["You", "Monster"],
       playerHealth: 65,
-      monsterHealth: 40
+      monsterHealth: 40,
     };
   },
   components: {
     Healthbar,
+    PlayerActions
   },
 };
 </script>
 
 <style>
 .inline {
-    display: inline-block;
-    width:48%;
-    margin: 1%;
+  display: inline-block;
+  width: 48%;
+  margin: 1%;
 }
 </style>
---
+++ src/components/playerActions.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-card class="mx-auto" max-width="100%">
+      <div align="center">
+        <v-btn class="my-3" large color="primary">Start</v-btn>
+      </div>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
