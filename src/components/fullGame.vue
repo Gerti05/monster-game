dbe25a3577a0192703522427f45cbc237f618496
Gerti Goga <gertigoga@gmail.com>
2020-06-09 03:46:44 -0700

    Added avatars for hero and monster.

--- src/components/fullGame.vue
+++ src/components/fullGame.vue
@@ -1,14 +1,31 @@
 <template>
   <div>
-    <Healthbar class="inline" :player="players[0]" :health="playerHealth" :special="playerSpecial"/>
-    <Healthbar class="inline" :player="players[1]" :health="monsterHealth" :special="monsterSpecial"/>
+    <Healthbar
+      class="inline"
+      :player="players[0]"
+      :health="playerHealth"
+      :special="playerSpecial"
+    />
+    <Healthbar
+      class="inline"
+      :player="players[1]"
+      :health="monsterHealth"
+      :special="monsterSpecial"
+    />
+    <v-container class="d-flex justify-space-between">
+      <Hero class="avatarBackground" />
+      <Monster class="avatarBackground" />
+    </v-container>
     <PlayerActions />
   </div>
 </template>
 
 <script>
 import Healthbar from "./healthbar";
-import PlayerActions from "./playerActions"
+import PlayerActions from "./playerActions";
+import Hero from "../assets/hero";
+import Monster from "../assets/monster";
+
 export default {
   data: function() {
     return {
@@ -16,12 +33,14 @@ export default {
       playerHealth: 65,
       monsterHealth: 40,
       playerSpecial: 0,
-      monsterSpecial: 100
+      monsterSpecial: 100,
     };
   },
   components: {
     Healthbar,
-    PlayerActions
+    PlayerActions,
+    Hero,
+    Monster,
   },
 };
 </script>
@@ -31,5 +50,8 @@ export default {
   display: inline-block;
   width: 48%;
   margin: 1%;
+}
+.avatarBackground {
+  background-color: #9575cd;
 }
 </style>
--- src/components/healthbar.vue
+++ src/components/healthbar.vue
@@ -5,7 +5,12 @@
         <h1 class="text-center textColor">{{ player }}</h1>
         <template>
           <div>
-            <v-progress-linear :value="health" color="light-green darken-1" height="25" rounded>
+            <v-progress-linear
+              :value="health"
+              color="light-green darken-1"
+              height="25"
+              rounded
+            >
               <template v-slot="{ value }">
                 <strong class="textColor">{{ Math.ceil(value) }}%</strong>
               </template>
@@ -18,7 +23,12 @@
       <v-col cols="6" class="pt-0">
         <template>
           <div>
-            <v-progress-linear :value="special" color="orange lighten-1" height="15" rounded>
+            <v-progress-linear
+              :value="special"
+              color="orange lighten-1"
+              height="15"
+              rounded
+            >
             </v-progress-linear>
           </div>
         </template>
